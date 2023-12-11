d8aa8276b0cb7aa95785089cc99a927df11c63b6
Paul Lerner <lerner@tuta.io>
2023-12-11 15:48:25 +0100

    draft: rm script from FH (gregarityNow) available at https://github.com/fyvo/neonyms_herron

--- scripts/FH/__init__.py
+++
@@ -1,9 +0,0 @@
-print("importing everythang")
-from .basis_funcs import *
-from .process_xml import *
-from .tokenizer import *;
-from .base_models import *;
-from .analyze_base_models import *;
-from .derif import *;
-from .pipelines import *;
-from .daille import *;
