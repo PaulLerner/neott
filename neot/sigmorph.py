596fed780953320441a257251776e13a73b2c6aa
Paul Lerner <lerner@tuta.io>
2024-01-09 18:34:05 +0100

    feat: sigmorph: detect neoclassical terms with morphynet affixes

--- neot/sigmorph.py
+++ neot/sigmorph.py
@@ -4,11 +4,31 @@
 import random
 import json
 from collections import Counter
+import enum
 
+import numpy as np
 import pandas as pd
 import seaborn as sns
 
 from .morph import Term, Inflected, Prefixed, Suffixed, Converted, Native, Neoclassical, Syntagm
+
+
+class MorphyType(enum.Enum):
+    prefix = 0
+    suffix = 1
+    
+    
+def get_morphy_table(lang="eng"):
+    table=pd.read_csv(f"/home/lerner/open/MorphyNet/{lang}/{lang}.derivational.v1.tsv","\t",names=["source word","target word","source POS","target POS","morpheme","type"])
+
+    morphy_affixes = {}
+    for t, subset in table.groupby("type"):
+        morphy_affixes[MorphyType[t]] = subset
+
+    morphy_prefixes = Counter(morphy_affixes[MorphyType.prefix].morpheme)
+
+    morphy_suffixes = Counter(morphy_affixes[MorphyType.suffix].morpheme)
+    return morphy_prefixes, morphy_suffixes
 
 
 def get_morph_table(lang="en"):
@@ -26,18 +46,13 @@ def get_morph_table(lang="en"):
     for i, k in enumerate(["Inflection","Derivation","Compound"]):
         table[k] = process[i]
     
-    compounds = table[~table.Inflection & ~table.Derivation & table.Compound]
-    compounds = Counter(m for morph in compounds.morpheme for m in morph.split(" @@"))
-    
-    derivations = table[~table.Inflection & table.Derivation & ~table.Compound]
-    prefixes = Counter(morph.split(" @@")[0] for morph in derivations.morpheme)
-    suffixes = Counter(morph.split(" @@")[-1] for morph in derivations.morpheme)
-    
     per_target = {}
     for target, sources in table.groupby("word"):
         per_target[target.lower().strip()]=sources
-    
-    return per_target, prefixes, suffixes, compounds
+        
+    prefixes, suffixes = get_morphy_table(lang)
+
+    return per_target, prefixes, suffixes
 
 
 def maybe_rec_get_morph(morphemes, original_token, front, from_morphemes=True, memory=None, *args, **kwargs):
@@ -65,9 +80,23 @@ def maybe_rec_get_morph(morphemes, original_token, front, from_morphemes=True, m
     return compound, morpheme
 
 
-def get_morph(token, spacy_pos=None, mwe=None, per_target=None, prefixes=None, suffixes=None, compounds=None, memory=None):
+def get_compound(token, morphemes, **kwargs):
+    if len(morphemes) > 2:
+        r, l = maybe_rec_get_morph(morphemes, token, front=True, **kwargs)
+        l = Term(l, trust=0.5)
+    else:
+        l, r = morphemes
+        l, r = Term(l, trust=0.5), Term(r, trust=0.5)
+    return l, r
+
+
+def is_neo_classical(morphemes, pos, prefixes, suffixes):
+    return pos not in {"ADP", "DET"} and all((m in prefixes or m in suffixes) for m in morphemes)
+
+
+def get_morph(token, pos=None, per_target=None, prefixes=None, suffixes=None, memory=None):
     if token not in per_target:
-        return Term(term=token, pos=spacy_pos, trust=0.0)
+        return Term(term=token, pos=pos, trust=0.0)
     
     memory.add(token)
     # empirically, there is always a single option
@@ -76,34 +105,38 @@ def get_morph(token, spacy_pos=None, mwe=None, per_target=None, prefixes=None, s
     
     # assumes that Inflection is always the right-most process
     if morph.Inflection:
-        compound, inflection = maybe_rec_get_morph(morphemes, token, front=False, per_target=per_target, prefixes=prefixes, suffixes=suffixes, compounds=compounds, memory=memory)           
+        compound, inflection = maybe_rec_get_morph(morphemes, token, front=False, per_target=per_target, prefixes=prefixes, suffixes=suffixes, memory=memory)           
         term = Inflected(term=token, inflection=inflection, stem=compound, trust=0.5)
     # before derivation
-    # TODO a lot of Neoclassical are tagged Derivation
     elif morph.Derivation:
-        p_freq = prefixes.get(morphemes[0], 0)
-        s_freq = suffixes.get(morphemes[-1], 0)
-        # the prefix is more frequent than the suffix OR (both equal or UNK but prefix shorther than suffix)
-        if (p_freq > s_freq) or ((p_freq == s_freq) and (len(morphemes[0]) < len(morphemes[-1]))):
-            compound, prefix = maybe_rec_get_morph(morphemes, token, front=True, per_target=per_target, prefixes=prefixes, suffixes=suffixes, compounds=compounds, memory=memory)
-            term = Prefixed(term=token, prefix=prefix, stem=compound, pos=spacy_pos, trust=0.5)
+        # catch neoclassical hidden in derivation
+        if is_neo_classical(morphemes, pos, prefixes, suffixes):
+            l, r = get_compound(token, morphemes, per_target=per_target, prefixes=prefixes, suffixes=suffixes, memory=memory)
+            term = Neoclassical(term=token, stem_l=l, stem_r=r, pos=pos, trust=0.5) 
+        # back to real affixes
         else:
-            compound, suffix = maybe_rec_get_morph(morphemes, token, front=False, per_target=per_target, prefixes=prefixes, suffixes=suffixes, compounds=compounds, memory=memory)
-            term = Suffixed(term=token, suffix=suffix, stem=compound, pos=spacy_pos, trust=0.5)
+            p_freq = prefixes.get(morphemes[0], 0)
+            s_freq = suffixes.get(morphemes[-1], 0)
+            # the prefix is more frequent than the suffix OR (both equal or UNK but prefix shorther than suffix)
+            if (p_freq > s_freq) or ((p_freq == s_freq) and (len(morphemes[0]) < len(morphemes[-1]))):
+                compound, prefix = maybe_rec_get_morph(morphemes, token, front=True, per_target=per_target, prefixes=prefixes, suffixes=suffixes, memory=memory)
+                term = Prefixed(term=token, prefix=prefix, stem=compound, pos=pos, trust=0.5)
+            else:
+                compound, suffix = maybe_rec_get_morph(morphemes, token, front=False, per_target=per_target, prefixes=prefixes, suffixes=suffixes, memory=memory)
+                term = Suffixed(term=token, suffix=suffix, stem=compound, pos=pos, trust=0.5)
     # and before Compounding (last process before root)
     elif morph.Compound:
-        if len(morphemes) > 2:
-            # assumes right-headed compound (English and Neoclassical)
-            r, l = maybe_rec_get_morph(morphemes, token, front=True, per_target=per_target, prefixes=prefixes, suffixes=suffixes, compounds=compounds, memory=memory)
-            l = Term(l, trust=0.5)
+        # catch neoclassical hidden in native compounds
+        neo_classical = is_neo_classical(morphemes, pos, prefixes, suffixes)
+        l, r = get_compound(token, morphemes, per_target=per_target, prefixes=prefixes, suffixes=suffixes, memory=memory)
+        if neo_classical:
+            term = Neoclassical(term=token, stem_l=l, stem_r=r, pos=pos, trust=0.5) 
         else:
-            l, r = morphemes
-            l, r = Term(l, trust=0.5), Term(r, trust=0.5)
-        term = Native(term=token, stem_l=l, stem_r=r, pos=spacy_pos, trust=0.5)        
+            term = Native(term=token, stem_l=l, stem_r=r, pos=pos, trust=0.5)        
     # monomorpheme
     else:
         assert len(morphemes) == 1
-        term = Term(token, pos=spacy_pos, trust=0.5)
+        term = Term(token, pos=pos, trust=0.5)
                
     return term
 
@@ -113,14 +146,14 @@ def parse_data(data, per_target, lang="en", **kwargs):
         term = item[lang]["text"].lower().strip()
         # init memory to avoid infinite recursion
         memory = set()
+        # prioritize morph tokenization over spacy's tokenization (equivalent for single-token terms)
         if term in per_target: 
-            term = get_morph(term, item[lang]["pos"][0], False, per_target=per_target, memory=memory, **kwargs)
+            term = get_morph(term, item[lang]["pos"][0], per_target=per_target, memory=memory, **kwargs)
         else:
             tokens = []
-            mwe = len(item[lang]["tokens"]) > 1
             for token, pos in zip(item[lang]["tokens"], item[lang]["pos"]):
                 token = token.lower().strip()
-                tokens.append(get_morph(token, pos, mwe, per_target=per_target, memory=memory, **kwargs))
+                tokens.append(get_morph(token, pos, per_target=per_target, memory=memory, **kwargs))
             term = Syntagm(terms=tokens, term=item[lang]["text"])
         # TODO .to_dict() to save to JSON
         item[lang]["morph"] = term
@@ -160,18 +193,19 @@ def viz(data, lang="en"):
     morphemes_l = pd.DataFrame(morphemes_l)
     fig = sns.displot(morphemes_l,x="length", hue="unit",discrete=True)
     fig.savefig(f"viz/FranceTerme_{lang}_morph_sig.pdf")
+    
+    indices = np.arange(len(data))
+    np.random.shuffle(indices)
+    for i in indices[:50]:
+        morph = data[i][lang]["morph"]
+        print(morph, morph.signature())
 
 
 if __name__ == "__main__":
     with open("data/FranceTerme_triples.json","rt") as file:
         data = json.load(file)
-    lang="en"
-    per_target, prefixes, suffixes, compounds = get_morph_table(lang=lang)
-    parse_data(data, per_target=per_target, prefixes=prefixes, suffixes=suffixes, compounds=compounds, lang=lang)
+    lang="fr"
+    per_target, prefixes, suffixes = get_morph_table(lang=lang)
+    parse_data(data, per_target=per_target, prefixes=prefixes, suffixes=suffixes, lang=lang)
     viz(data, lang)
     save(data, lang)
-
-    random.shuffle(data)
-    for item in data[:50]:
-        morph = item[lang]["morph"]
-        print(morph, morph.signature())
