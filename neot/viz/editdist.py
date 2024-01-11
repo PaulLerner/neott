fd51da1df10bcda2f260c9962222e344b7116a81
Paul Lerner <lerner@tuta.io>
2024-01-11 15:55:12 +0100

    feat: viz: editdist: CLI

--- neot/viz/editdist.py
+++ neot/viz/editdist.py
@@ -1,20 +1,38 @@
-import seaborn as sns
-import editdistance
 import json
+from tqdm import tqdm
+from jsonargparse import CLI
 #import random
 
+import pandas as pd
+import seaborn as sns
 
-with open("data/FranceTerme_triples.json","rt") as file:
-    data = json.load(file)
+import editdistance
+
     
-distances = []
-#random.shuffle(data)
-for triple in data:
-    editd = editdistance.eval(triple['fr']["text"],triple['en']["text"])
-    #if editd not in distances:
-    #    print(editd,triple['fr'],triple['en'])
-    distances.append(editd)
+def edit_dist(data):
+    distances = []
+    #random.shuffle(data)
+    for triple in tqdm(data):
+        editd = editdistance.eval(triple['fr']["text"],triple['en']["text"])
+        #if editd not in distances:
+        #    print(editd,triple['fr'],triple['en'])
+        distances.append(editd)
+    return pd.DataFrame({"distance": distances})
 
-fig = sns.displot(distances,discrete=True)
 
-fig.savefig("viz/FranceTerme_editdist.pdf")
+def viz(distances, output):
+    desc = distances.describe(percentiles=[.25, .5, .75, .9, .95, .99])
+    print(desc)
+    fig = sns.displot(distances[distances.distance < desc.distance["99%"]], discrete=True)    
+    fig.savefig(output)
+    
+    
+def main(data: str, output: str):
+    with open(data,"rt") as file:
+        data = json.load(file)
+    distances = edit_dist(data)
+    viz(distances, output)
+    
+    
+if __name__ == "__main__":
+    CLI(main)
