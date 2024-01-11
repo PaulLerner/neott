84cc3155386c5ae7446ef305c8e23b3866c0d3ed
Paul Lerner <lerner@tuta.io>
2024-01-11 17:08:59 +0100

    feat: utils: register pathlib Path in jsonargparse

--- neot/utils.py
+++ neot/utils.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from jsonargparse.typing import register_type
+from pathlib import Path
 import numpy as np
+
+
+register_type(Path, type_check=lambda v, t: isinstance(v, t))
+
 
 def random_data(data, n=None):
     if n is None:
@@ -9,3 +15,4 @@ def random_data(data, n=None):
     np.random.shuffle(indices)
     for i in indices[:n]:
         yield data[i]
+        
