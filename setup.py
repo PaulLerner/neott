cd695d5965b696a6c525450d316a16a299e953fb
Paul Lerner <lerner@tuta.io>
2024-01-15 18:56:13 +0100

    chore: add requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-from setuptools import setup
+import os
 
+import pkg_resources
+from setuptools import setup
 
 setup(
     name='neot',
-    packages=['neot']
+    packages=['neot'],
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ]
 )
