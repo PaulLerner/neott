609094112e2ee144339605a49dc63134fd1f0899
Paul Lerner <lerner@tuta.io>
2024-01-26 17:45:44 +0100

    feat: classif: predict

--- neot/morph/classif.py
+++ neot/morph/classif.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
+from collections import Counter
 
 import pandas as pd
 import numpy as np
@@ -15,46 +16,63 @@ from ..utils import Path
 
 class Classifier:
     """Multi-label classifier for neologisms morphology"""
-    def __init__(self, train_path: Path, dev_path: Path, test_path: Path, model_path: Path = None):
-        self.train_path = train_path
-        self.dev_path = dev_path
-        self.test_path = test_path
+    def __init__(self, model_path: str = None):
         self.model = None if model_path is None else fasttext.load_model(model_path)
 
-    def train(self):
+    def train(self, train_path: Path, dev_path: Path, test_path: Path = None):
+        """Train"""
         assert self.model is None
         # hyper param tuning
-        self.model = fasttext.train_supervised(input=str(self.train_path), loss='ova', autotuneValidationFile=str(self.dev_path))
+        self.model = fasttext.train_supervised(input=str(train_path), loss='ova', autotuneValidationFile=str(dev_path))
         hyperparameters = {k: v for k, v in self.model.__dict__.items() if isinstance(v, (float, int))}
         print(hyperparameters)
-        with open(self.train_path.parent / "hyperparameters.json", "wt") as file:
+        with open(train_path.parent / "hyperparameters.json", "wt") as file:
             json.dump(hyperparameters, file)
-        self.model.save_model(str(self.train_path.parent / "model.bin"))
+        self.model.save_model(str(train_path.parent / "model.bin"))
 
-        self.test()
+        if test_path is not None:
+            self.test(test_path)
 
-    def test(self):
-        results = self.model.test_label(str(self.test_path), k=-1, threshold=0.5)
-        _, p, r = self.model.test(str(self.test_path), k=-1, threshold=0.5)
+    def test(self, test_path: Path):
+        """Test"""
+        results = self.model.test_label(str(test_path), k=-1, threshold=0.5)
+        _, p, r = self.model.test(str(test_path), k=-1, threshold=0.5)
         f = (2 * p * r) / (p + r)
         results["overall"] = {'precision': p,
                               'recall': r,
                               'f1score': f}
         print((pd.DataFrame(results) * 100).T.to_latex(float_format="{:.1f}".format))
 
-        self.viz()
+        self.viz(test_path)
 
-    def viz(self):
+    def viz(self, test_path: Path):
+        """Visualize"""
         precision, recall = [], []
         for threshold in np.arange(0.0, 1.01, 0.01):
-            _, p, r = self.model.test(str(self.test_path), k=-1, threshold=threshold)
+            _, p, r = self.model.test(str(test_path), k=-1, threshold=threshold)
             precision.append(p)
             recall.append(r)
 
         p_v_r = pd.DataFrame(dict(precision=precision, recall=recall))
         fig = sns.relplot(p_v_r, x="precision", y="recall", kind="line", markers=True)
-        fig.savefig(self.test_path.parent/"p_v_r.pdf")
+        fig.savefig(test_path.parent / "p_v_r.pdf")
         return fig
+
+    def predict(self, predict_path: Path, lang: str):
+        """Predict on OOD data"""
+        with open(predict_path, 'rt') as file:
+            data = json.load(file)
+        labels = Counter()
+        for subset in data.values():
+            for item in subset:
+                label = [l[len('__label__'):] for l in self.model.predict(item[lang]['text'], k=-1, threshold=0.5)[0]]
+                if len(item[lang]["tokens"]) > 1:
+                    label.append("Syntagm")
+                item[lang]["morph_label"] = label
+                labels[" ".join(sorted(label))] += 1
+        print(labels.most_common())
+        with open(predict_path, 'wt') as file:
+            json.dump(data, file)
 
 
 if __name__ == "__main__":
