92c15a11a57ef113264fafe2e8c7a6fa63d1b08a
Paul Lerner <lerner@tuta.io>
2024-01-31 15:06:26 +0100

    feat: freq: option to handle OSCAR-like JSON files

--- neot/freq.py
+++ neot/freq.py
@@ -2,9 +2,13 @@
 import json
 from jsonargparse import CLI
 from collections import Counter
+from tqdm import tqdm
+import pandas as pd
 
 import datasets
 import ahocorasick
+
+from .utils import Path
 
 
 def build_automaton(terms):
@@ -21,15 +25,22 @@ def count(batch, automaton, counter):
         counter[original_value] += 1
 
 
-def main(glossary: str, corpus: str, output: str, lang: str = "fr"):
+def main(glossary: str, corpus: Path, output: str, lang: str = "fr", hf: bool = True, batch_size: int = 100000):
     """Compute the frequency of terms in glossary on a given corpus"""
     with open(glossary, "rt") as file:
         glossary = json.load(file)
-    terms = set(item[lang]["text"].lower().strip() for item in glossary)
+    terms = set(item[lang]["text"].lower().strip() for subset in glossary.values() for item in subset)
+    print(f"{len(terms)=}")
     automaton = build_automaton(terms)
-    corpus = datasets.load_from_disk(corpus)
     counter = Counter()
-    corpus.map(count, input_columns="text", batched=True, fn_kwargs=dict(automaton=automaton, counter=counter))
+    if hf:
+        corpus = datasets.load_from_disk(corpus)
+        corpus.map(count, input_columns="text", batched=True, batch_size=batch_size, fn_kwargs=dict(automaton=automaton, counter=counter))
+    else:
+        for input_path in tqdm(list(corpus.glob('*.jsonl'))):
+            texts = pd.read_json(input_path, lines=True).content
+            for b in range(0, len(texts), batch_size):
+                count(texts[b: b+batch_size], automaton, counter)
     with open(output, "wt") as file:
         json.dump(counter, file)
 
