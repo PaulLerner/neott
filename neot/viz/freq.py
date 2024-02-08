23f928eba40343effb8460c67cc5de06fc2a960c
Paul Lerner <lerner@tuta.io>
2024-02-08 14:27:45 +0100

    feat: viz.freq: combine counters + show random example + show how many zeros

--- neot/viz/freq.py
+++ neot/viz/freq.py
@@ -3,12 +3,16 @@
 
 import json
 from collections import Counter
+from typing import Union, List
+
 from jsonargparse import CLI
 
 import numpy as np
 import pandas as pd
 
 import seaborn as sns
+
+from neot.utils import ListOrArg
 
 
 def viz(freq, output):
@@ -18,18 +22,26 @@ def viz(freq, output):
     freq_k = np.array(list(freq.keys()))
     deciles = np.quantile(freq_v, np.arange(0, 1.1, 0.1), method="nearest")
 
-    for v in deciles:
-        i = np.where(freq_v == v)[0][0]
-        print(freq_k[i], "&", v, r"\\")
+    equal_zero = len(np.where(freq_v == 0)[0])
+    print(equal_zero, equal_zero/len(freq_k))
+    for decile, v in zip(np.arange(0, 1.1, 0.1), deciles):
+        indices = np.where(freq_v == v)[0]
+        np.random.shuffle(indices)
+        print(decile, "&", freq_k[indices[0]], "&", v, r"\\")
 
     # shift by 1 to plot zero values in log-scale
     fig = sns.displot(freq_v+1, bins=100, log_scale=True)
     fig.savefig(output)
 
 
-def main(freq: str, output: str):
-    with open(freq, "rt") as file:
-        freq = Counter(json.load(file))
+def main(output: str, freq_paths: Union[str, List[str]] = None):
+    freq_paths = ListOrArg(freq_paths)
+    freq = Counter()
+    for freq_path in freq_paths:
+        with open(freq_path, "rt") as file:
+            for k, v in Counter(json.load(file)).items():
+                # Counter.__add__ will remove 0
+                freq[k] += v
     viz(freq, output)
 
 
