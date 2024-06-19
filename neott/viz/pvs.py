d3d189b3cc39ce901f9fba2c4b922b94ad172408
Paul Lerner <lerner@tuta.io>
2024-06-19 15:08:43 +0200

    fix: viz.pvs

--- neott/viz/pvs.py
+++ neott/viz/pvs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 import itertools
+import warnings
+
 from jsonargparse import CLI
 import enum
 
@@ -38,11 +40,6 @@ def compute_accuracy(argmax, sorted_vocab, start_indices, intra_indices, START_O
 
 def compute_alignment(model_name: str, word_embeddings, vocab, START_OF_WORD_CHAR, alpha_filter: bool = False,
                       negatives: Negatives = Negatives.intra_pair):
-
-    norm = word_embeddings.norm(2, 1, keepdim=True)
-    word_embeddings /= norm
-    print(f"{word_embeddings.shape=} {norm.shape=}")
-
     not_word = re.compile(r"[^A-Za-z]")
     starts = {token[1:] for token in vocab if token[0] == START_OF_WORD_CHAR and ((not alpha_filter) or not_word.search(token[1:]) is None)}
     pairs = starts & vocab.keys()
@@ -59,13 +56,10 @@ def compute_alignment(model_name: str, word_embeddings, vocab, START_OF_WORD_CHA
 
     if negatives == Negatives.all_intra:
         intra_indices = torch.tensor([v for k, v in vocab.items() if k[0] != START_OF_WORD_CHAR], dtype=int)
-        num_negatives = len(intra_indices)-1
     elif negatives == Negatives.all:
         intra_indices = torch.arange(len(word_embeddings), dtype=int)
-        num_negatives = len(intra_indices)-1-len(start_indices)
     else:
         intra_indices = torch.tensor(intra_indices, dtype=int)
-        num_negatives = len(intra_indices)-1
 
     print(f"{len(start_indices)=} {len(intra_indices)=}")
 
@@ -73,7 +67,7 @@ def compute_alignment(model_name: str, word_embeddings, vocab, START_OF_WORD_CHA
     intra_embeddings = word_embeddings[intra_indices]
     sim = start_embeddings @ intra_embeddings.T
 
-    print(f"{len(sim.shape)=} {sum(sim.shape)=}")
+    print(f"{sim.shape=} {sum(sim.shape)=}")
 
     # remove self from top-2
     if negatives == Negatives.all:
@@ -85,19 +79,30 @@ def compute_alignment(model_name: str, word_embeddings, vocab, START_OF_WORD_CHA
 
     print("model | filter `A-Za-z` | #pairs | negatives | P@1 (cs) | P@1 (ci) | matches other start")
     print("------|-----------------|--------|-----------|----------|----------|--------------------")
-    print(f"{model_name} | {alpha_filter} | {len(pairs)} | {num_negatives} {negatives.name} | ", end=" ")
+    print(f"{model_name} | {alpha_filter} | {len(pairs)} | {len(intra_indices)-1} {negatives.name} | ", end=" ")
     compute_accuracy(argmax, sorted_vocab, start_indices, intra_indices, START_OF_WORD_CHAR)
 
 
 def main(model_name: str, alpha_filter: bool = None, negatives: Negatives = None):
-    model = AutoModelForCausalLM.from_pretrained(model_name)
-    word_embeddings = model.model.embed_tokens.weight.detach().to(torch.float32)
-
     tokenizer = AutoTokenizer.from_pretrained(model_name, add_prefix_space=True)
     vocab = tokenizer.vocab
     print(tokenizer.tokenize("foo", " foo"))
     START_OF_WORD_CHAR = tokenizer.tokenize("foo")[0][0]
     print(f"{START_OF_WORD_CHAR=} {len(vocab)=}")
+
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    # FIXME get_input_embeddings
+    if hasattr(model, "model"):
+        word_embeddings = model.model.embed_tokens.weight
+    else:
+        word_embeddings = model.transformer.word_embeddings.weight
+    word_embeddings = word_embeddings.detach().to(torch.float32)
+    if len(word_embeddings) > len(vocab):
+        warnings.warn(f"Trimming {word_embeddings.shape=} to {len(vocab)=}")
+        word_embeddings = word_embeddings[:len(vocab)]
+    norm = word_embeddings.norm(2, 1, keepdim=True)
+    word_embeddings /= norm
+    print(f"{word_embeddings.shape=} {norm.shape=}")
 
     alpha_filter = [alpha_filter] if alpha_filter is not None else [False, True]
     negatives = [negatives] if negatives is not None else Negatives
