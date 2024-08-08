35afbb034b5a921581bc16cde27f334fc9aa494e
Paul Lerner <lerner@tuta.io>
2024-08-08 11:03:08 +0200

    refactor: move load_lightning function from save_ptm to utils

--- neott/save_ptm.py
+++ neott/save_ptm.py
@@ -3,21 +3,12 @@ from jsonargparse import CLI
 import yaml
 
 from . import trainee
-from .utils import Path
+from .utils import Path, load_lightning
 
 
 def main(ckpt: Path = None, config_path: Path = None):
     """Save the PreTrainedModel(s) wrapped inside the Trainee (LightningModule)."""
-    if config_path is None:
-        assert ckpt is not None, "you must provide either ckpt or config (or both)"
-        config_path = ckpt.parent.parent / 'config.yaml'
-    with open(config_path, 'rt') as file:
-        config = yaml.load(file, yaml.Loader)
-    if ckpt is None:
-        ckpt = Path(config["ckpt_path"])
-    class_name = config['model']['class_path'].split('.')[-1]
-    Class = getattr(trainee, class_name)
-    model = Class.load_from_checkpoint(ckpt, **config['model']['init_args'])
+    model = load_lightning(ckpt=ckpt, config_path=config_path)
     ckpt_path = ckpt.with_suffix('')
     model.model.save_pretrained(ckpt_path)
 
--- neott/utils.py
+++ neott/utils.py
@@ -9,6 +9,41 @@ import numpy as np
 register_type(Path, type_check=lambda v, t: isinstance(v, t))
 
 
+def load_lightning(ckpt: Path = None, config_path: Path = None):
+    """
+    Lighning experiments are usually organized as follows:
+
+    root
+    ├── lightning_logs
+    │   └── version_47
+    │       ├── checkpoints
+    │       │   └── step=912.ckpt <- provide with ckpt
+    │       └── config.yaml       <- will resolve to this config
+    └── test.yaml                 <- OR provide with config_path (with a "ckpt_path" key)
+
+    Parameters
+    ----------
+    ckpt: Path, optional
+        e.g. root/lightning_logs/checkpoints/step=912.ckpt above
+        config_path will then be set to ../../config.yaml -> root/lightning_logs/config.yaml
+    config_path: Path, optional (exclusive of ckpt)
+        e.g. root/test.yaml above
+        If you already have configured (e.g. for test/validation/predict) with a "ckpt_path" key
+    """
+
+    if config_path is None:
+        assert ckpt is not None, "you must provide either ckpt or config (or both)"
+        config_path = ckpt.parent.parent / 'config.yaml'
+    with open(config_path, 'rt') as file:
+        config = yaml.load(file, yaml.Loader)
+    if ckpt is None:
+        ckpt = Path(config["ckpt_path"])
+    class_name = config['model']['class_path'].split('.')[-1]
+    Class = getattr(trainee, class_name)
+    model = Class.load_from_checkpoint(ckpt, **config['model']['init_args'])
+    return model
+
+
 def load_json_line(path):
     with open(path, "rt") as file:
         for line in file.readlines():
@@ -19,6 +54,7 @@ def random_data(data, n=None):
     if n is None:
         n = len(data)
     assert n > 0
+    # TODO refactor with choice
     indices = np.arange(len(data))
     np.random.shuffle(indices)
     for i in indices[:n]:
