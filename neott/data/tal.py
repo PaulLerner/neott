cb723a9e0e4e2d6f639ee0ffc6da074f573e1899
Paul Lerner <lerner@tuta.io>
2024-09-05 14:58:00 +0200

    feat: data: parse TAL version POC

--- neott/data/tal.py
+++ neott/data/tal.py
@@ -4,40 +4,59 @@
 
 import json
 import pandas as pd
-
+import re
 
 def get_data():
-    data = pd.read_csv("../data/TAL/terms/Terminologie_TAL/TerminologieTAL_tous_les_termes.csv", delimiter=";")
+    data = pd.read_csv("/home/paul/code/WP1-Resources-and-Corpora/TerminologieTAL/TerminologieTALpoc.csv",delimiter=";")
 
     data = data.dropna(subset=["skos:prefLabel@fr", "skos:prefLabel@en"])
-
-    # sum(x.lower() == y.lower() for x,y in zip(data["skos:hiddenLabel@fr"], data["skos:prefLabel@fr"]))
 
     def get_if_not_nan(row, key):
         if row[key] == row[key]:
             return row[key]
         return None
 
+    def get_fr(string):
+        if string is None:
+            return None
+
+        assert string.startswith('"')
+        assert string.endswith('"@fr')
+        return string[1: -4]
+
+    def get_alt_fr(string):
+        return re.findall(r'"(.+?)"@fr', string) if '##' not in string else re.split('##', get_fr(string))
+
+    def get_en(string):
+        if string is None:
+            return None
+        assert string.startswith('"')
+        assert string.endswith('"@en')
+        return string[1: -4]
+
+    def get_alt_en(string):
+        return re.findall(r'"(.+?)"@en', string) if '##' not in string else re.split('##', get_en(string))
+
     triples = []
     for i, row in data.iterrows():
         fr_terms = get_if_not_nan(row, "skos:altLabel@fr")
-        fr_terms = fr_terms.split("##") if fr_terms is not None else None
+        fr_terms = [{"text": fr_term} for fr_term in get_alt_fr(fr_terms)] if fr_terms is not None else None
         en_terms = get_if_not_nan(row, "skos:altLabel@en")
-        en_terms = en_terms.split("##") if en_terms is not None else None
+        en_terms = [{"text": en_term} for en_term in get_alt_en(en_terms)] if en_terms is not None else None
         triples.append({
             "Dom": None,
             "S-dom": None,
             "fr": {
-                "text": row["skos:prefLabel@fr"],
-                "def": {"text": get_if_not_nan(row, "skos:definition@fr")},
+                "text": get_fr(row["skos:prefLabel@fr"]),
+                "def": {"text": get_fr(get_if_not_nan(row, "skos:definition@fr"))},
                 "syn": fr_terms
             },
             "en": {
-                "text": row["skos:prefLabel@en"],
-                "def": {"text": get_if_not_nan(row, "skos:definition@en")},
+                "text": get_en(row["skos:prefLabel@en"]),
+                "def": {"text": get_en(get_if_not_nan(row, "skos:definition@en"))},
                 "syn": en_terms
             },
-            "id": row['skos:exactMatch'] if row['skos:exactMatch'] == row['skos:exactMatch'] else f"TAL_{i}"
+            "id": f"TAL_{i}"
         })
     return triples
 
@@ -45,5 +64,5 @@ def get_data():
 if __name__ == '__main__':
     data = get_data()
 
-    with open("../data/TAL/terms/TAL.json", "wt") as file:
+    with open("./data/TAL/terms/TAL_poc.json", "wt") as file:
         json.dump(data, file)
